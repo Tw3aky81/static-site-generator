1c879acaa0dcef2a43e668c67ad7656c3d30a4d8
Francis Engels <tw3aky@gmail.com>
2026-06-02 20:50:05 +0200

    add TextNode class

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    tn = TextNode(
+        "This is some anchor text", TextType.LINK_TYPE, "https://www.boot.dev"
+    )
+    print(tn)
+
+
+if __name__ == "__main__":
+    main()
