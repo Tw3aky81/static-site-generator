a055ccb2b2ec18dab19b570133e339b07a9062ac
Francis Engels <tw3aky@gmail.com>
2026-06-03 19:57:19 +0200

    add textNode to HTMLNode converter
    Adding a function to convert TextNodes to HTMLNodes, LeafNodes
    specifically. (CH2-L6)

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     PLAIN = "text"
@@ -27,3 +29,21 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.PLAIN:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": f"{text_node.url}"})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode(
+            "img", "", {"src": f"{text_node.url}", "alt": f"{text_node.text}"}
+        )
+    raise RuntimeError("Cannot convert Textnode to HTMLNode")
