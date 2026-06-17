4d47e123e05f6064a253cfbd37d78df2ebcf8cc7
Francis Engels <tw3aky@gmail.com>
2026-06-18 00:11:30 +0200

    correction: solution CH3-L4

--- src/textconverter.py
+++ src/textconverter.py
@@ -27,11 +27,11 @@ def split_nodes_delimiter(
 
 
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
-    return re.findall(r"!\[(.+?)\]\((.+?)\)", text)
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
-    return re.findall(r"\[(.+?)\]\((.+?)\)", text)
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
 if __name__ == "__main__":
