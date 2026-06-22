f6abdb37a9a376fbcadfdc585e0fd82504e6da5e
Francis Engels <tw3aky@gmail.com>
2026-06-22 18:12:01 +0200

    adding blocktype enum and block_to_blocktype function
    Adding a BlockType enum and function to determine which type a block is.
    (CH4-L2)

--- src/block.py
+++ src/block.py
@@ -1,3 +1,16 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH_TYPE = "paragraph"
+    HEADING_TYPE = "heading"
+    CODE_TYPE = "code"
+    QUOTE_TYPE = "quote"
+    UL_TYPE = "unordered_list"
+    OL_TYPE = "ordered_list"
+
+
 def makdown_to_blocks(markdown: str) -> list[str]:
     blocks = []
     raw_blocks = markdown.split("\n\n")
@@ -6,3 +19,17 @@ def makdown_to_blocks(markdown: str) -> list[str]:
         if block != "":
             blocks.append(block)
     return blocks
+
+
+def block_to_block_type(block: str) -> BlockType:
+    if re.search(r"^#{1,6} .+$", block):
+        return BlockType.HEADING_TYPE
+    if re.search(r"^`{3}\n[\S\s]+`{3}$", block, re.MULTILINE):
+        return BlockType.CODE_TYPE
+    if re.search(r"^> ?.+$", block, re.MULTILINE):
+        return BlockType.QUOTE_TYPE
+    if re.search(r"^- .+$", block, re.MULTILINE):
+        return BlockType.UL_TYPE
+    if re.search(r"^\d\. .+$", block, re.MULTILINE):
+        return BlockType.OL_TYPE
+    return BlockType.PARAGRAPH_TYPE
--- src/test_block.py
+++ src/test_block.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block import makdown_to_blocks
+from block import BlockType, block_to_block_type, makdown_to_blocks
 
 
 class TestBlock(unittest.TestCase):
@@ -29,11 +29,11 @@ This is the same paragraph on a new line
 This is **bolded** paragraph      
 
 
-This is another paragraph with _italic_ text and `code` here
+       This is another paragraph with _italic_ text and `code` here
 This is the same paragraph on a new line
 
 
-
+      
 
 
 - This is a list
@@ -48,3 +48,35 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_paragraph_blocktype(self):
+        block = "This is an ordinary paragraph block"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.PARAGRAPH_TYPE)
+
+    def test_block_to_heading_blocktype(self):
+        block = "# This is a heading 1"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.HEADING_TYPE)
+
+    def test_block_to_code_blocktype(self):
+        block = "```\nThis is a multiline\ncode block\nwith code\n```"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.CODE_TYPE)
+
+    def test_block_to_quote_blocktype(self):
+        block = (
+            "> This is a multiline\n>quote block\n> with optional spaces after bracket"
+        )
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.QUOTE_TYPE)
+
+    def test_block_to_ul_blocktype(self):
+        block = "- This is a multiline\n- unordered list block\n- with items"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.UL_TYPE)
+
+    def test_block_to_ol_blocktype(self):
+        block = "1. This is a multiline\n2. ordered list block\n3. with items"
+        blocktype = block_to_block_type(block)
+        self.assertEqual(blocktype, BlockType.OL_TYPE)
